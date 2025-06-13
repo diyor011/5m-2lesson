4239d4858caa3e6a93ef52d553adcd00dfc8599f
diyor011 <104572938+diyor011@users.noreply.github.com>
2025-06-13 17:03:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// data type in js
+
+
+
+// number
+// string
+// boolean
+// simbool
+// undefined
+// null
+//  object
+//  bigint
+
+
+
+
+
+let string = prompt('String tupedigi malumot kiriting')
+let number = prompt('Number tupedigi malumot kiriting')
+let boolean = prompt('Boolean tupedigi malumot kiriting')
+
+let javob = confirm("Hamma ma'lumotlarni to'g'ri kiritganingizga ishonchingiz komilmi?");
+
+
+
+alert(
+    "Siz kiritgan " + string + " so‘zingiz string turiga tegishli\n" +
+    "Siz kiritgan " + number + " so‘zingiz number turiga tegishli\n" +
+    "Siz kiritgan " + boolean + " so‘zingiz boolean turiga tegishli"
+  );
+  
+
